99cb210ac7f4ead7b0765359dd7b033ac1ec1d07
Maria Isabel <contacto.mims@gmail.com>
2023-02-12 13:36:17 +0100

    Creados objetos de Administrativo y Alumno

--- Actividad_06/src/Testing/Principal.java
+++ Actividad_06/src/Testing/Principal.java
@@ -1,9 +1,17 @@
 package Testing;
 
+import modelo.javabeans.Administrativo;
+import modelo.javabeans.Alumno;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		 * Se crean los objetos de Administrativo, Alumno y Profesor
+		 */
+		Administrativo admin = new Administrativo("123456789G", "Mariano García", "Calle Londres, 39", "666778899", "Papeleo");
+		Alumno alu = new Alumno("987654321U", "Pablo Gomez", "Calle Praga, 89", "777889966", "Segundo de Bachillerato");
+		
 
 	}
 
